5a7a702d9e2581b296f2cbf095ce8d679359580d
Shashank <shashanksagar398@gmail.com>
2025-05-06 01:31:25 +0530

    feat: reorder imports in main.tsx for improved readability

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,15 @@ import { useLocalStorage } from '@mantine/hooks';
 import { lightTheme, darkTheme } from './theme/theme';
 import { useThemeStore } from './store/theme.store';
 import App from './App';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
+
 import ShipsList from './pages/ships/ships-listing/ShipsList';
 import ShipDetail from './pages/ships/ship-details/ShipDetail';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './components/ui-components/not-found/NotFound';
 import RocketsList from './pages/rockets/RocketList';
 import RocketDetail from './pages/rockets/RocketDetails';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 export const routes = [
 	{
